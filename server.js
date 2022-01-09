d8b8669c93fb57245420065e33423d26106816c7
NQNam <namtv27072001@gmail.com>
2022-01-09 21:40:31 +0700

    updated

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(cors());
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-// app.use('/posts', posts);
+app.use('/', (req , res) => {
+  res.json("Success")
+});
 
 mongoose
   .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
