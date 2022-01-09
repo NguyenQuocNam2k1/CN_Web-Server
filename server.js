e340db0e200d171af1772407be62eecacd7f8058
NQNam <namtv27072001@gmail.com>
2022-01-09 21:48:20 +0700

    Test API

--- server.js
+++ server.js
@@ -1,32 +1,72 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 // import posts from './routers/posts.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const URI = "mongodb+srv://admin:admin123@cluster0.xvdds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const URI =
+  "mongodb+srv://admin:admin123@cluster0.xvdds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 app.use(cors());
-app.use(express.json({ limit: '30mb' }));
-app.use(express.urlencoded({ extended: true, limit: '30mb' }));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 
-app.get('/', (req , res) => {
-  res.json("Success")
+app.get("/", (req, res) => {
+  res.json("Success");
 });
 
 mongoose
   .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to DB');
+    console.log("Connected to DB");
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log('err', err);
+    console.log("err", err);
   });
+
+
+  
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      default: "Anonymous",
+    },
+    attachment: String,
+    likeCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const myBlog = mongoose.model("post", blogSchema);
+
+app.get("/posts", async (req, res) => {
+  try {
+    const posts = await myBlog.find({});
+    console.log("Post", posts);
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
