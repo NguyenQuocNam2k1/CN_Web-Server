811760b3825897eb109d9489313ef3700558347e
NQNam <namtv27072001@gmail.com>
2022-01-11 16:40:53 +0700

    fix server

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(cors());
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-// app.use('/', (req , res) => {
-//   res.json("Success")
-// });
+app.get('/', (req , res) => {
+  res.json("Success")
+});
 
 app.use('/api/user', users);
 
