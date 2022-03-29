011395165fad2fdd33f7fddd7ce0fa3045542994
NQNam <namtv27072001@gmail.com>
2022-03-29 15:53:41 +0700

    _

--- config/socketIO.js
+++ config/socketIO.js
@@ -1,16 +1,16 @@
 const cors = require("cors");
-const moment = require('moment');
+const moment = require("moment");
 const { commentModel } = require("../models/CommentModel");
 
 module.exports = (server) => {
   const io = require("socket.io")(server, {
     cors: {
       origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
+      methods: ["GET", "POST"],
     },
   });
   io.on("connection", (socket) => {
-    console.log("New client connected" + socket.id);
+    // console.log("New client connected" + socket.id);
 
     // Thằng này dùng để join vào từng lesson cho user
     socket.on("join_room", (data) => {
@@ -18,17 +18,29 @@ module.exports = (server) => {
       console.log(`User with ID: ${socket.id} joined room: ${data}`);
     });
 
+    //Thằng này dùng để lấy tất cả các comment theo khóa học
+    socket.on("get_comment", (idRoom) => {
+      commentModel
+        .find({ idRoom })
+        .then((cmt) => {
+          socket.emit("receive_all_comment", cmt);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+
     // Thằng này dùng để nhận cmt từ người dùng , lưu comment
-    socket.on("send_comment", data => {
-      socket.to(data.idRoom).emit("receive_comment", data);
-      console.log(`${data.idRoom} co ${data.idUser} gui tin`);
-      /* const comment = new commentModel(cmt);
-      comment.save(function (err, res) {
-        // Dòng bên dưới dùng để check nếu như lưu cmt bị failed
-        if (err) console.log("SERVER ERROR");
-        // Dòng bên dưới dùng để trả về cmt cho room
-        socket.in(room).emit("receive_comment", res);
-      }); */
+    socket.on("send_comment", (data) => {
+      commentModel.create(data)
+      .then(data => {
+        console.log(data);
+        socket.emit("receive_comment", data);
+        socket.in(data.idRoom).emit("receive_comment", data);
+      })
+      .catch(err => {
+        console.log("ERROR: ", err)
+      })
     });
 
     socket.on("update_content", (room, _id, content) => {
@@ -43,20 +55,26 @@ module.exports = (server) => {
       );
     });
 
-    // socket.on("update_count_like", (room , _id , data) => {
-    //   commentModel.findByIdAndUpdate(
-    //     _id,
-    //     {countLike: data},
-    //     { new: true },
-    //     function (err, res) {
-    //       if (err) console.log(err);
-    //       socket.in(room).emit("receive_count_like_updated", res);
-    //     }
-    //   );
-    // });
-    socket.on('updateLike', (data) => {
-      socket.to(data.idRoom).emit("receive_count_like_updated", (data));
-      console.log('data: ', data);
+    socket.on("update_count_like", (data) => {
+      commentModel
+        .findOneAndUpdate(
+          { _id: data.idCmt },
+          { countLike: data.countLike }
+        )
+        .then((res) => {
+          commentModel
+            .find({ idRoom:data.room })
+            .then((cmt) => {
+              socket.emit("receive_all_comment", cmt);
+              socket.to(data.room).emit("receive_all_comment", cmt);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log("Error:", err);
+        });
     });
 
     // Tạo 1 cmt mà cmt này trả lời 1 cmt khác
